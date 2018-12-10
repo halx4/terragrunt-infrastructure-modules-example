77797c950d82a4faeda9f52f508e4fa7979fd7be
foivos <foivoschrist@outlook.com>
2018-12-10 22:00:52 +0000

    add test ami module

---
+++ ami/main.tf
@@ -0,0 +1,18 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# A SIMPLE EXAMPLE OF HOW DEPLOY AN ASG WITH AN ELB IN FRONT OF IT
+# This is an example of how to use Terraform to deploy an Auto Scaling Group (ASG) with an Elastic Load
+# Balancer (ELB) in front of it. To keep the example simple, we deploy a vanilla Ubuntu AMI across the ASG and run a
+# dirt simple "web server" on top of it as a User Data script. The "web server" always returns "Hello, World".
+#
+# Note: This code is meant solely as a simple demonstration of how to lay out your files and folders with Terragrunt
+# in a way that keeps your Terraform code DRY. This is not production-ready code, so use at your own risk.
+# ---------------------------------------------------------------------------------------------------------------------
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+}
---
+++ ami/vars.tf
@@ -0,0 +1,4 @@
+variable "owner-id" {
+  description = "The AWS region to deploy to (e.g. us-east-1)"
+  default="099720109477"
+}
