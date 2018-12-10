a59cd1429481fc6a2b1852b2ba4acbe7b2ca6114
foivos <foivoschrist@outlook.com>
2018-12-10 22:42:42 +0000

    add ec2 module

---
+++ ec2/main.tf
@@ -0,0 +1,44 @@
+# ---------------------------------------------------------------------------------------------------------------------
+# A SIMPLE EXAMPLE OF HOW DEPLOY AN ASG WITH AN ELB IN FRONT OF IT
+# This is an example of how to use Terraform to deploy an Auto Scaling Group (ASG) with an Elastic Load
+# Balancer (ELB) in front of it. To keep the example simple, we deploy a vanilla Ubuntu AMI across the ASG and run a
+# dirt simple "web server" on top of it as a User Data script. The "web server" always returns "Hello, World".
+#
+# Note: This code is meant solely as a simple demonstration of how to lay out your files and folders with Terragrunt
+# in a way that keeps your Terraform code DRY. This is not production-ready code, so use at your own risk.
+# ---------------------------------------------------------------------------------------------------------------------
+
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+terraform {
+  # The configuration for this backend will be filled in by Terragrunt
+  backend "s3" {}
+}
+
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-trusty-14.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami = "${data.aws_ami.ubuntu.id}"
+  instance_type = "t2.micro"
+
+  tags {
+    Name = "HelloWorld12"
+  }
+}
