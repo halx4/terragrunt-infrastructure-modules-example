60615aeda73e32dd48b23b111e0278cace7651ed
foivos <foivoschrist@outlook.com>
2018-12-10 23:19:45 +0000

    add vpc parameter

--- ec2/main.tf
+++ ec2/main.tf
@@ -37,6 +37,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "web" {
   ami = "${data.aws_ami.ubuntu.id}"
   instance_type = "t2.micro"
+  vpc_security_group_ids = ["${var.security_group}"]
 
   tags {
     Name = "HelloWorld12"
--- ec2/vars.tf
+++ ec2/vars.tf
@@ -1,3 +1,7 @@
 variable "aws_region" {
   description = "The AWS region to deploy to (e.g. us-east-1)"
 }
+
+variable "security_group" {
+
+}
